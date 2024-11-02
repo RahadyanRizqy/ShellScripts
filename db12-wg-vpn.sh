8385abd0ee86cf2be2d246525ebadaa086ed5017
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2024-11-03 00:00:44 +0700

    Create deb12-wg-vpn.sh

---
+++ db12-wg-vpn.sh
@@ -0,0 +1,41 @@
+apt update
+apt install git -y
+apt-get install sudo git iptables -y && \
+sudo apt-get update && \
+sudo apt install wireguard-tools net-tools && \
+git clone https://github.com/donaldzou/WGDashboard.git && \
+cd ./WGDashboard/src && \
+chmod +x ./wgd.sh && \
+./wgd.sh install && \
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && \
+sudo sysctl -p /etc/sysctl.conf
+
+wg genkey | tee /etc/wireguard/private.key | wg pubkey > /etc/wireguard/public.key
+prikey=$(cat /etc/wireguard/private.key)
+
+cat << EOF > /etc/wireguard/wg0.conf
+[Interface]
+PrivateKey = $prikey
+Address = 10.255.255.1/24
+ListenPort = 65533
+SaveConfig = true
+DNS = 1.1.1.1, 8.8.8.8
+
+PostUp = sysctl -q -w net.ipv4.ip_forward=1
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = sysctl -q -w net.ipv4.ip_forward=0
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+EOF
+
+chmod 600 /etc/wireguard/wg0.conf
+
+echo "WireGuard configuration created at: /etc/wireguard/wg0.conf"
+
+sed -i 's/10086/65534/g' static/app/proxy.js
+sed -i 's/10086/65534/g' dashboard.py
+sed -i 's/^Port 22/Port 65535/' /etc/ssh/sshd_config
+
+cd /root/WGDashboard/src
+./wgd.sh start
+
+sudo systemctl restart sshd
